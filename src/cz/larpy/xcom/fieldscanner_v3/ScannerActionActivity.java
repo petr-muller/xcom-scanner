ca20f4f4c585e085a360763c20eee31d1dddb850
Petr Muller <muller@redhat.com>
2013-09-27 03:34:49 +0200

    Add an Activity to show info after Scanner Action

---
+++ src/cz/larpy/xcom/fieldscanner_v3/ScannerActionActivity.java
@@ -0,0 +1,21 @@
+package cz.larpy.xcom.fieldscanner_v3;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.app.Activity;
+
+public class ScannerActionActivity extends Activity {
+
+	public static final String ACTIVITY = "ACTIVITY";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_scanner_action);
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+	}
+}
